ab86f514575db3d56bdae7754fa78e9c62644a89
seunghyunlee <kailyyy02@gmail.com>
2021-06-11 18:18:28 +0900

    "도서 관리 시스템 간단 작업"

---
+++ book/src/main/java/com/spring/service/BookService.java
@@ -0,0 +1,15 @@
+package com.spring.service;
+
+import java.util.List;
+
+import com.spring.domain.BookVO;
+
+public interface BookService {
+	
+	public int insert(BookVO vo);
+	public int delete(int code);
+	public int update(int code, int price);
+	public List<BookVO> list();
+	public List<BookVO> search(String criteria, String keyword);
+
+}
