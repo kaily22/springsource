4dfeeb07fb4d75840eae24b42023348d8ec11c2f
seunghyunlee <kailyyy02@gmail.com>
2021-06-14 23:18:05 +0900

    "Restcontroller를 사용하여 도서관리 시스템 데이터 받아오기"

---
+++ book_rest/src/main/java/com/spring/controller/HomeController.java
@@ -0,0 +1,38 @@
+package com.spring.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import lombok.extern.log4j.Log4j2;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+@Log4j2
+public class HomeController {
+
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String index() {
+		log.info("index ø‰√ª");
+
+		return "index";
+	}
+
+	@RequestMapping(value = "/bookrest", method = RequestMethod.GET)
+	public void bookrest() {
+		log.info("rest");
+	}
+
+	@RequestMapping(value = "/book-insert", method = RequestMethod.GET)
+	public String bookInsert() {
+		log.info("rest insert");
+		return "insertForm";
+	}
+
+}
