cc0664e1c8cc69961ade0c1fd79366fff0e60162
seunghyunlee <kailyyy02@gmail.com>
2021-06-23 23:25:59 +0900

    "simple_board javaconfig 사용"

---
+++ simple_board_cp_mybatis_javaconfig/src/main/java/com/spring/mapper/BoardMapper.java
@@ -0,0 +1,15 @@
+package com.spring.mapper;
+
+import java.util.List;
+
+import com.spring.domain.BoardVO;
+
+public interface BoardMapper {
+
+	public int insert(BoardVO vo);
+	public int update(BoardVO vo);
+	public int delete(int bno);
+	public BoardVO read(int bno);
+	public List<BoardVO> list();
+	
+}
